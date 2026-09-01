9b966745aa377c880584693c1734825594c8c189
JEET AHIRWAR <jeetahirwar664@gmail.com>
2026-09-01 17:09:41 +0530

    master

--- Frontend-Admin-panel/src/data/school.js
+++ Frontend-Admin-panel/src/data/school.js
@@ -27,5 +27,5 @@ export const currentUser = {
   name: "Jeet Ahirwar",
   role: "Administrator",
   email: "jeet.ahirwar@brightwoodschool.edu.in",
-  avatar: "https://drive.google.com/file/d/1VTdXtl5ElEf8UhMa2Cw6khrlt1ZliU-B/view?usp=sharing",
+  avatar: "../public/Passportsize-photo-Jeet.jpeg",
 };
